6e6852bdb5c1491799d9fd90222dda61413cf889
Ayoub Ouabi <ayoub.ouabi06@gmail.com>
2023-03-14 22:06:15 +0100

    create models and tables

---
+++ database/migrations/2023_03_14_205301_create_livres_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('livres', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('genre_id');
+            $table->foreign('genre_id')->references('id')->on('genres');
+            $table->string('collection');
+            $table->string('isbn');
+            $table->date('released_date');
+            $table->integer('page_numbers');
+            $table->string('emplacement');
+            $table->string('statut');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('livres');
+    }
+};
